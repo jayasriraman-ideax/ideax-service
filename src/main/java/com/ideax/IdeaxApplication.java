efb3681d89504f28d21098ec4788cb8dc3372895
jayasriraman-ideax <jayasriraman.ideax@gmail.com>
2026-02-28 09:14:43 +0530

    Project Code Commit

---
+++ src/main/java/com/ideax/IdeaxApplication.java
@@ -0,0 +1,13 @@
+package com.ideax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideax/IdeaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
